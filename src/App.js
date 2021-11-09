29a45f56e80814b0a77c8bc261ec03428ec25087
ligia.bozzi@gmail.com <ligia.bozzi@gmail.com>
2021-11-09 08:54:06 -0300

    implementing layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,166 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header'>
+        <div className='text-header'>
+        <h1>Green Food</h1>
+        <p className='subtitle'>Comida saudável na mesa da sua casa</p>
+        </div>
+        <img src='images/logo_green.png'></img>
+      </div>
+
+
+      <h3>Primeiro, escolha o seu prato</h3>
+
+      <ul className="list">
+
+        <li class="option selected">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="option ">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="option">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+      </ul>
+
+      <h3>Agora, escolha a sua bebida</h3>
+
+      <ul className="list">
+
+        <li class="option selected">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="option ">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="option">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+      </ul>
+
+      <h3>Por fim, a desejada sobremesa</h3>
+
+      <ul className="list">
+
+        <li class="option selected">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="option ">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="option">
+          <img src="images/frango.jpg" />
+          <h4>Frango Feliz</h4>
+          <p className='description'>Frango criado livre</p>
+          <div className='details'>
+            <p className='price'>R$ 15.90</p>
+            <div className='quantity'>
+              <button className='add'>-</button>
+              <span>1</span>
+              <button className='remove'>+</button>
+            </div>
+          </div>
+        </li>
+
+      </ul>
+
+      <div className='close-order'>
+        <button className='able'>
+          Fechar Pedido
+        </button>
+      </div>
+    </>
   );
 }
 
