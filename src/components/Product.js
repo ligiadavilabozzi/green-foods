c1e8c2df532594180aa66e39e62eee89c1745841
ligia.bozzi@gmail.com <ligia.bozzi@gmail.com>
2021-11-09 11:04:23 -0300

    implementing button quantities

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
 export default function Product(props) {
-    
-    const { product, changeQuantity} = props
+
+    const { product, changeQuantity } = props
+    const { imgUrl, name, description, quantity, price } = product
 
     function selectItem() {
-        if(product.quantity === 0){
+        if (quantity === 0) {
             changeQuantity(product, 1)
         }
     }
 
     return (
         <>
-            <li className={product.quantity > 0 ? 'option selected' : 'option'} onClick={selectItem}>
-                <img src={product.imgUrl} />
-                <h4>{product.name}</h4>
-                <p className='description'>{product.description}</p>
-                <p className='price'>R$ {product.price.toFixed(2)}
+            <li className={quantity > 0 ? 'option selected' : 'option'} onClick={selectItem}>
+                <img src={imgUrl} />
+                <h4>{name}</h4>
+                <p className='description'>{description}</p>
+                <p className='price'>R$ {price.toFixed(2)}
                     <div className='quantity'>
-                        <button className='remove' >-</button>
-                        <span>{product.quantity}</span>
-                        <button className='add' >+</button>
+                        <button className='remove' onClick={() => changeQuantity(product, quantity - 1)}>-</button>
+                        <span>{quantity}</span>
+                        <button className='add' onClick={() => changeQuantity(product, quantity + 1)}>+</button>
                     </div>
                 </p>
 
